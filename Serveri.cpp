b245ef4f42b8e7a456506c6559f8a8fa8365906c
DielliDoci <dielli.doci@student.uni-pr.edu>
2024-11-16 17:56:54 +0100

    logjika e socketave/winsock

--- Serveri.cpp
+++ Serveri.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <winsock2.h>
-#include <ws2tcpip.h>
+#include <ws2tcpip.h> // ka funksionalitete edhe per UDP
 #include <fstream>
 #include <string>
 using namespace std;
@@ -20,4 +20,40 @@ void handleWriteRequest(const string& data) {
     } else {
         cerr << "Unable to open file for writing on the server.\n";
     }
+}
+
+int main(){
+    WSADATA wsaData;
+    SOCKET serverSocket;
+    sockaddr_in serverAddr, clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+    char buffer[BUFLEN];
+
+    // Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "WSAStartup failed. Terminating.\n";
+        return 1;
+    }
+
+    // Krijimi i socketave
+    serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverSocket == INVALID_SOCKET) {
+        cerr << "Socket creation failed. Error: " << WSAGetLastError() << "\n";
+        WSACleanup();
+        return 1;
+    }
+    // Struktura e adreses se serverit
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cerr << "Bind failed. Error: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Server is running and waiting for commands...\n";
 }
